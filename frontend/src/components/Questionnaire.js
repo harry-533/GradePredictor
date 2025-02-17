0eb3d04c34bb10f19103119f7d274615509aeab4
harry-533 <harry.sharpmail@yahoo.com>
2025-02-17 10:45:45 +0000

    commit files after deploying backend

--- frontend/src/components/Questionnaire.js
+++ frontend/src/components/Questionnaire.js
@@ -53,7 +53,7 @@ function Questionnaire() {
 
     const handleSubmit = async () => {
         try {
-            const response = await fetch("http://127.0.0.1:5000/predict", {
+            const response = await fetch("https://gradepredictor.onrender.com", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
