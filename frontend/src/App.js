513474dd0a5005a05b55ef39ae13bb1501efa37c
harry-533 <110621027+harry-533@users.noreply.github.com>
2025-02-17 11:14:18 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
     const [data, setData] = useState("");
 
     useEffect(() => {
-        axios.get("http://127.0.0.1:5000/api/data")
+        axios.get("https://gradepredictor.onrender.com/api/data")
             .then(response => setData(response.data.data))
             .catch(error => console.error("Error fetching data:", error));
     }, []);
